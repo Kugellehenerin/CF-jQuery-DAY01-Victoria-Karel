ac45f1e20d13dfdb85ef131c75bfe8d7132f50b8
Victoria Kahr <victoria.kahr@icloud.com>
2020-10-05 14:56:02 +0200

    advanced

---
+++ advanced/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('a').attr('href', 'www.codefactory.wien');
+    $('code').css("color", "red");
+    $('ol > li').css("background-color", "#2a7b90");
+    $('textarea').text("Express your opinion");
+    $('img').attr('src', 'https://codefactory.wien/wp-content/themes/hestia/cf-images/big-jpg/1-frontend.jpg')
+    $('blockquote').css({
+        "background-color": "orange",
+        'font-style': 'italic'
+    });
+    $('#logo').css("color", "black");
+    $('.gray').css("color", "white");
+    $('.date').remove();
+    var templates = $("<li>New Template</li>");
+    var order = $("<li>Other Template</li>");
+    var contact = $("<li>Contact US</li>");
+    $('ul:nth-child(2)').prepend(templates, order, contact);
+    var text = $("I want a programming language in which I need only to say what I wish, and done', give him a lollipop.")
+    $('#content-wrap').text().replace("Be not afraid of life. Believe that life is worth living, and your belief will help create the fact.", "I want a programming language in which I need only to say what I wish, and done.");
+
+});
